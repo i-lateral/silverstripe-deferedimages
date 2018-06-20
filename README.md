0ae19c8a82abac3fac521c29fcf07771aafada5c
Makreig <mark@i-lateral.com>
2018-06-20 14:56:01 +0100

    added parser for content images

--- README.md
+++ README.md
@@ -6,9 +6,20 @@ By default all images called in the templates using the standard `$Image.Type()`
 The temporary image can be customised in size, quality and appearance via config:
 
 ```
-i-lateral\SilverStripe\DeferedImages\DeferedImage:
+ilateral\SilverStripe\DeferedImages\DeferedImage:
     pixelate: 15 // Applies a pixelation effect to the current image with a given size of pixels.
     blur: 50 // Apply a gaussian blur filter with a optional amount on the current image. Use values between 0 and 100.
     quality: 10  // Define the quality of the encoded image. Data ranging from 0 to 100.
-    scale: 100 // Will resize the image by a percentage amount.
+    scale: 100 // Will resize the image to a percentage amount. 100 = current size, 50 = half size, etc.
+```
+
+To enable deferring of images added in a `HTMLEditorField` you will need to enable the included shortcode parser via _config.php:
+
+```
+<?php
+use SilverStripe\View\Parsers\ShortcodeParser;
+use ilateral\SilverStripe\DeferedImages\DeferedImageShortcodeProvider;
+
+ShortcodeParser::get('default')
+    ->register('image', [DeferedImageShortcodeProvider::class, 'handle_shortcode']);
 ```
---
+++ src/DeferedImageShortcodeProvider.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace ilateral\SilverStripe\DeferedImages;
+
+use SilverStripe\Dev\Debug;
+use SilverStripe\View\HTML;
+use SilverStripe\Assets\Image;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Assets\Storage\AssetStore;
+use SilverStripe\Assets\Shortcodes\ImageShortcodeProvider;
+
+class DeferedImageShortcodeProvider extends ImageShortcodeProvider
+{
+    /**
+     * Replace"[image id=n]" shortcode with an image reference.
+     * Permission checks will be enforced by the file routing itself.
+     *
+     * @param array $args Arguments passed to the parser
+     * @param string $content Raw shortcode
+     * @param ShortcodeParser $parser Parser
+     * @param string $shortcode Name of shortcode used to register this handler
+     * @param array $extra Extra arguments
+     * @return string Result of the handled shortcode
+     */
+    public static function handle_shortcode($args, $content, $parser, $shortcode, $extra = array())
+    {
+        $cache = static::getCache();
+        $cacheKey = static::getCacheKey($args);
+
+        $item = $cache->get($cacheKey);
+        if ($item) {
+            /** @var AssetStore $store */
+            $store = Injector::inst()->get(AssetStore::class);
+            if (!empty($item['filename'])) {
+                $store->grant($item['filename'], $item['hash']);
+            }
+            return $item['markup'];
+        }
+
+        // Find appropriate record, with fallback for error handlers
+        $record = static::find_shortcode_record($args, $errorCode);
+        if ($errorCode) {
+            $record = static::find_error_record($errorCode);
+        }
+        if (!$record) {
+            return null; // There were no suitable matches at all.
+        }
+
+        // Check if a resize is required
+        $src = $record->Link();
+        $micro_url = false;
+        if ($record instanceof Image) {
+            $width = isset($args['width']) ? $args['width'] : null;
+            $height = isset($args['height']) ? $args['height'] : null;
+            $hasCustomDimensions = ($width && $height);
+            if ($hasCustomDimensions && (($width != $record->getWidth()) || ($height != $record->getHeight()))) {
+                $resized = $record->ResizedImage($width, $height);
+                // Make sure that the resized image actually returns an image
+                if ($resized) {
+                    $src = $resized->getURL();
+                }
+            }
+            $micro_url = $record->MicroImage()->getURL();
+        }
+
+        // Build the HTML tag
+        $attrs = array_merge(
+            // Set overrideable defaults
+            ['src' => '', 'alt' => $record->Title],
+            // Use all other shortcode arguments
+            $args,
+            // But enforce some values
+            ['id' => '', 'src' => $src]
+        );
+
+        // Clean out any empty attributes
+        $attrs = array_filter($attrs, function ($v) {
+            return (bool)$v;
+        });
+
+        if ($micro_url) {
+            $attrs['data-src'] = $src;
+            $attrs['src'] = $micro_url;
+        }
+
+        $markup = HTML::createTag('img', $attrs);
+
+        // cache it for future reference
+        $cache->set($cacheKey, [
+            'markup' => $markup,
+            'filename' => $record instanceof File ? $record->getFilename() : null,
+            'hash' => $record instanceof File ? $record->getHash() : null,
+        ]);
+
+        return $markup;
+    }
+}
