2b3a168af9c53e4d9de2abb4c8c71a5139503a02
Morven Lewis-Everley <morven@ilateral.co.uk>
2018-08-08 10:49:36 +0100

    Enclose defered images code in a function

--- client/dist/js/deferedimages.js
+++ client/dist/js/deferedimages.js
@@ -1,16 +1,18 @@
-// script thanks to: https://varvy.com/pagespeed/defer-images.html
-function imagedefer() {
-    var imgDefer = document.getElementsByTagName('img');
-    for (var i=0; i<imgDefer.length; i++) {
-        if(imgDefer[i].getAttribute('data-src')) {
-            imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-        } 
+(function(window) {
+    // script thanks to: https://varvy.com/pagespeed/defer-images.html
+    function imagedefer() {
+        var imgDefer = document.getElementsByTagName('img');
+        for (var i=0; i<imgDefer.length; i++) {
+            if(imgDefer[i].getAttribute('data-src')) {
+                imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+            }
+        }
     }
-}
-
-if (window.addEventListener) {
-    window.addEventListener('load', imagedefer, false);
-} else if (window.attachEvent) {
-    // Microsoft
-    window.attachEvent('onload', imagedefer);
-}
+    
+    if (window.addEventListener) {
+        window.addEventListener('load', imagedefer, false);
+    } else if (window.attachEvent) {
+        // Microsoft
+        window.attachEvent('onload', imagedefer);
+    }
+}(window));
